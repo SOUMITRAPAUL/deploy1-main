020c1800b768c95cf163152c5ba885e67a4faeb0
unknown <SOUMITRAPAUL>
2025-01-07 03:30:32 +0600

    update

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import server from './app.js';  
-import request from 'supertest';  
-import assert from 'assert';  
+import assert from 'assert';
+import request from 'supertest';
+import server from './app.js';
 
 
 describe('GET /', () => {
   it('should return Hello World', async () => {
     const response = await request(server).get('/');
     console.log(response.text);  
-    assert.strictEqual(response.text, 'Hello World Pass');
+    assert.strictEqual(response.text, 'Hello World abc');
   });
 });
